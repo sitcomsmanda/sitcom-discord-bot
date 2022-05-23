9785a07cafbf3766638b7fb7d9444f6d975b7941
DikDns <dikdnssocial@gmail.com>
2022-05-23 14:34:09 +0700

    add napi-rs/canvas and remove static images from embed

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -15,7 +15,7 @@ module.exports = {
       .setDescription(
         `Selamat datang di server discord\n**SITCOM** (Student IT Community)\n\nDiharapkan untuk membaca **Peraturan** di <#978124379013644298> terlebih dahulu!\n\nDilanjutkan ke <#978124379013644298> untuk berkenalan sesuai format.\n\nJika ada pertanyaan, jangan malu untuk bertanya kepada __Ketua__`
       )
-      .setImage("https://i.imgur.com/AfFp7pu.png")
+      .setImage(message.author.avatarURL({ size: 256 }))
       .setTimestamp()
       .setFooter({
         text: `${message.author.username}`,
