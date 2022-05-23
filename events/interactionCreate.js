b719aa42e9a36b32dc94d63b0949255074bcd3b2
DikDns <dikdnssocial@gmail.com>
2022-05-23 11:05:32 +0700

    add interactionCreate event

---
+++ events/interactionCreate.js
@@ -0,0 +1,25 @@
+// Distribusi module events dengan nama event "interactionCreate"
+module.exports = {
+  // Nama Event: interactionCreate
+  name: "interactionCreate",
+  // kondisi once
+  once: false,
+  // Method isi Events ready
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  },
+};
