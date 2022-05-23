80380bde16e3cc60c574e02f8c69f3ada16cb90c
DikDns <dikdnssocial@gmail.com>
2022-05-23 17:12:55 +0700

    add embeds to welcomeMessage

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 // Distribusi module events dengan nama event "guildMemberAdd"
 // Yaitu ketika ada member masuk server
 module.exports = {
@@ -5,10 +7,25 @@ module.exports = {
   name: "guildMemberAdd",
   // Method isi Event guildMemberAdd
   async execute(member) {
-    console.log(member.guild.channels);
-    member.guild.channels.cache
-      .get("912507318929858600")
-      .send(`Selamat datang ${member.user}!`);
+    console.log(member.user);
+
+    const welcomeEmbed = new MessageEmbed()
+      .setColor("#07C966")
+      .setTitle(`Halo ${member.user.username}!`)
+      .setDescription(
+        `Selamat datang di server discord\n**SITCOM** (Student IT Community)\n\nDiharapkan untuk membaca **Peraturan** di <#978124379013644298> terlebih dahulu!\n\nDilanjutkan ke <#978124379013644298> untuk berkenalan sesuai format.\n\nJika ada pertanyaan, jangan malu untuk bertanya kepada __Ketua__`
+      )
+      .setImage(member.user.displayAvatarURL())
+      .setTimestamp()
+      .setFooter({
+        text: `${member.user.username}`,
+        iconURL: `${member.user.displayAvatarURL()}`,
+      });
+
+    member.guild.channels.cache.get("912507318929858600").send({
+      content: `Selamat datang ${member.user}!`,
+      embeds: [welcomeEmbed],
+    });
   },
 };
 // member.name untuk ngambil nama server
