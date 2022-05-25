179a00e2caffa15d6fae4a0e18d91d46f5f6b171
DikDns <dikdnssocial@gmail.com>
2022-05-25 17:07:10 +0700

    change using try and catch

--- modules/deploy_commands.js
+++ modules/deploy_commands.js
@@ -7,7 +7,7 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
 // !Fungsi deploy commands ke API Discord
-const deploy_commands = async (mode = "dev") => {
+const deploy_commands = async () => {
   // Inisialisasi object RESTful API version dengan token
   const rest = new REST({ option: "9" }).setToken(TOKEN);
 
@@ -21,28 +21,40 @@ const deploy_commands = async (mode = "dev") => {
     commands.push(command.data.toJSON());
   });
 
-  // jika dalam mode development (pengembangan)
-  if (mode === "build") {
-    // Request .put() dengan opsi input commands di global
-    await rest
-      .put(Routes.applicationCommands(CLIENT_ID), {
-        body: commands,
-      })
-      .then(() =>
-        console.log("Successfully registered application commands globally.")
-      )
-      .catch(console.error);
-  } else {
-    // Request .put() dengan opsi input commands di local server discord
-    await rest
-      .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-        body: commands,
-      })
-      .then(() =>
-        console.log("Successfully registered application commands locally.")
-      )
-      .catch(console.error);
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    console.log("Successfully reloaded application (/) commands.");
+    return rest;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
+
+  // jika dalam mode development (pengembangan)
+  // if (mode === "build") {
+  //   // Request .put() dengan opsi input commands di global
+  //   await rest
+  //     .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+  //       body: commands,
+  //     })
+  //     .then(() =>
+  //       console.log("Successfully registered application commands globally.")
+  //     )
+  //     .catch(console.error);
+  // } else {
+  //   // Request .put() dengan opsi input commands di local server discord
+  //   await rest
+  //     .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+  //       body: commands,
+  //     })
+  //     .then(() =>
+  //       console.log("Successfully registered application commands locally.")
+  //     )
+  //     .catch(console.error);
+  // }
 };
 
 module.exports = deploy_commands;
