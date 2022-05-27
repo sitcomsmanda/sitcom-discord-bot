5c6df82f45f9fb06434d8aaf29438483c81e586b
DikDns <dikdnssocial@gmail.com>
2022-05-27 10:18:48 +0700

    add details about user profile with dollar command

--- dollarCommands/profile.js
+++ dollarCommands/profile.js
@@ -1,5 +1,9 @@
-// const { MessageEmbed } = require("discord.js");
-// const Builders = require("@discordjs/builders");
+require("dotenv").config();
+
+const { MessageEmbed } = require("discord.js");
+const { time } = require("@discordjs/builders");
+
+const GUILD_ID = process.env.GUILD_ID;
 
 module.exports = {
   data: {
@@ -7,13 +11,38 @@ module.exports = {
     desc: "mengenai bot ini",
   },
   async execute(message) {
-    let text = ``;
-    text += `👀 `;
-    text += `Bot ini dikembangkan oleh anak-anak SITCOM SMA Negeri 2 Bandung `;
-    text += `dengan alasan kemandirian agar tidak bergantung kepada bot lain.`;
-    text += `\n`;
+    const guild = await message.author.client.guilds.cache.get(GUILD_ID);
+    const user = await message.author;
+    const members = await guild.members.search({ query: user.username });
+    const member = await members.get(user.id);
 
-    await message.channel.send({ content: text });
+    const embed = new MessageEmbed()
+      .setColor(`${member.displayHexColor}`)
+      .setFields(
+        {
+          name: `ID`,
+          value: `${member.user.id}`,
+        },
+        {
+          name: `Username`,
+          value: `${member.user.username}`,
+        },
+        {
+          name: `Nickname`,
+          value: `${member.displayName}`,
+        },
+        {
+          name: `Account Created`,
+          value: `${time(member.user.createdAt, "F")}`,
+        },
+        {
+          name: `Join ${member.guild.name} At`,
+          value: `${time(member.joinedAt, "F")}`,
+        }
+      )
+      .setThumbnail(`${member.user.avatarURL({ format: "png", size: 64 })}`);
+
+    await message.channel.send({ embeds: [embed] });
     return;
   },
 };
