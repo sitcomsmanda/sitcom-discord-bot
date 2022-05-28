9b4f93e099c30102f689d0a3c72700d2d345edc8
DikDns <dikdnssocial@gmail.com>
2022-05-28 07:16:38 +0700

    add music command builder

--- commands/music.js
+++ commands/music.js
@@ -4,9 +4,49 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("music")
-    .setDescription("Play a song!"),
-  async execute(interaction) {
-    await interaction.reply("Pong!");
-    await interaction.followUp("with slash(/) ping.");
-  },
+    .setDescription("Music system")
+    // music play
+    .addSubcommand((subcommand) => {
+      subcommand
+        .setName("play")
+        .setDescription("Play a song")
+        .addStringOption((option) => {
+          option
+            .setName("query")
+            .setDescription("Provide a name or a url for the song")
+            .setRequired(true);
+        });
+    })
+    // music volume
+    .addSubcommand((subcommand) => {
+      subcommand
+        .setName("volume")
+        .setDescription("Alter the volume")
+        .addNumberOption((option) => {
+          option
+            .setName("percent")
+            .setDescription("10 - 100%")
+            .setRequired(true);
+        });
+    })
+    // music setting
+    .addSubcommand((subcommand) => {
+      subcommand
+        .setName("setting")
+        .setDescription("Select an option")
+        .addStringOption((option) => {
+          option
+            .setName("options")
+            .setDescription("Select an option")
+            .setRequired(true)
+            .addChoices(
+              { name: "queue", value: "queue" },
+              { name: "skip", value: "skip" },
+              { name: "pause", value: "pause" },
+              { name: "resume", value: "resume" },
+              { name: "stop", value: "stop" }
+            );
+        });
+    }),
+  async execute(interaction) {},
 };
