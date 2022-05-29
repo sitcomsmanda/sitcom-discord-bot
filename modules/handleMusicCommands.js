33fbf2a630e2e4b5d50e2b80aae8e83da26496f2
DikDns <dikdnssocial@gmail.com>
2022-05-29 09:13:20 +0700

    add cleanMessage function

--- modules/handleMusicCommands.js
+++ modules/handleMusicCommands.js
@@ -2,24 +2,47 @@ require("dotenv").config();
 
 const PREFIX = process.env.PREFIX;
 
+// Fungsi untuk membersihkan pesan command music
+const cleanMessage = (oldContent) => {
+  let newContents = ``;
+  oldContent.split(" ").forEach((msg, i) => {
+    if (msg !== "") {
+      newContents += i !== 0 ? " " : "";
+      newContents += msg;
+    }
+  });
+  console.log(newContents);
+
+  return newContents;
+};
+
 // Fungsi Dolar Utama yang dipanggil di messageCreate Event
-const handleMusicCommands = (message) => {
+const handleMusicCommands = async (message) => {
+  const { author, client } = message;
+
   // Pencegah masuk function tanpa PREFIX
   if (!(message.content[0] === PREFIX)) return;
 
+  // Membersihkan spasi yang tidak guna
+  message.content = cleanMessage(message.content);
+
   // Menampilkan User yang menggunakan command
-  console.log(`${message.author.username}: ${message.content}`);
+  console.log(`${author.username}: ${message.content}`);
+
+  // Mengambil hanya index ke-0 dari rentetan pesan yang di split spasi
+
+  const contentCommand = message.content.split(" ")[0];
 
   // Mengambil musicCommand di client collection
-  const musicCommand = message.client.musicCommands.get(
+  const musicCommand = client.musicCommands.get(
     // Substring untuk hanya mengambil kata pertama
-    message.content.substring(0)
+    contentCommand
   );
 
   // Pencegah masuk function tanpa ada musicCommand yang sesuai
   if (!musicCommand) return;
 
-  musicCommand.execute(message);
+  await musicCommand.execute(message);
 
   return;
 };
