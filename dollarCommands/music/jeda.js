552ff85b4131870e2aff513ad265befcb73435dd
DikDns <dikdnssocial@gmail.com>
2022-05-30 09:10:43 +0700

    add pause 'jeda' command

---
+++ dollarCommands/music/jeda.js
@@ -0,0 +1,63 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  data: {
+    name: "jeda",
+    desc: "menghentikan sementara musik yang sedang diputar",
+  },
+  async execute(message) {
+    const { client, guild, member } = message;
+    const voiceChannel = member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("RED")
+            .setDescription(
+              `❗ | Kamu harus berada dalam \`voice channel\` untuk menghentikan musik`
+            ),
+        ],
+      });
+    }
+
+    if (
+      guild.me.voice.channelId &&
+      voiceChannel.id !== guild.me.voice.channelId
+    ) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("YELLOW")
+            .setDescription(
+              `❗ | Aku sedang memutar musik di <#${guild.me.voice.channelId}>.`
+            ),
+        ],
+      });
+    }
+
+    const queue = await client.distube.getQueue(voiceChannel);
+
+    if (!queue) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`🚫 | Tidak ada musik yang sedang diputar`),
+        ],
+      });
+    }
+
+    const song = queue.songs.at(0);
+
+    await queue.pause(voiceChannel);
+
+    return message.channel.send({
+      embeds: [
+        new MessageEmbed()
+          .setColor("#07C966")
+          .setDescription(`⏸️ | Menghentikan sementara \`${song.name}\``),
+      ],
+    });
+  },
+};
