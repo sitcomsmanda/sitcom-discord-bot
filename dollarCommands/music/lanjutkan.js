eb2593e4cae20e4aefc970a87e48ed43a072eb0e
DikDns <dikdnssocial@gmail.com>
2022-05-30 09:20:34 +0700

    add resume 'lanjutkan' command

---
+++ dollarCommands/music/lanjutkan.js
@@ -0,0 +1,63 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  data: {
+    name: "lanjutkan",
+    desc: "melanjutkan lagi musik yang dijeda",
+  },
+  async execute(message) {
+    const { client, guild, member } = message;
+    const voiceChannel = member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("RED")
+            .setDescription(
+              `❗ | Kamu harus berada dalam \`voice channel\` untuk melanjutkan musik`
+            ),
+        ],
+      });
+    }
+
+    if (
+      guild.me.voice.channelId &&
+      voiceChannel.id !== guild.me.voice.channelId
+    ) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("YELLOW")
+            .setDescription(
+              `❗ | Aku sedang memutar musik di <#${guild.me.voice.channelId}>.`
+            ),
+        ],
+      });
+    }
+
+    const queue = await client.distube.getQueue(voiceChannel);
+
+    if (!queue) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`🚫 | Tidak ada musik yang sedang diputar`),
+        ],
+      });
+    }
+
+    const song = queue.songs.at(0);
+
+    await queue.resume(voiceChannel);
+
+    return message.channel.send({
+      embeds: [
+        new MessageEmbed()
+          .setColor("#07C966")
+          .setDescription(`▶️ | Melanjutkan \`${song.name}\``),
+      ],
+    });
+  },
+};
