82a653bccb8f67059c87e2e02afb57d225f2fa27
DikDns <dikdnssocial@gmail.com>
2022-06-10 15:25:52 +0700

    remove status

--- events/distube/addList.js
+++ events/distube/addList.js
@@ -1,4 +1,3 @@
-const status = require("../../modules/distubeStatus.js");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
@@ -6,12 +5,11 @@ module.exports = {
   async execute(queue, playlist) {
     await queue.textChannel.send({
       embeds: [
-        new MessageEmbed().setColor("#43E97B").setDescription(
-          `↩️ | Memasukan daftar putar
-             \`${playlist.name}\` (${
-            playlist.songs.length
-          } songs) ke antrean\n${status(queue)}`
-        ),
+        new MessageEmbed()
+          .setColor("#43E97B")
+          .setDescription(
+            `↩️ | Memasukan daftar putar \`${playlist.name}\` (${playlist.songs.length} lagu) ke antrean`
+          ),
       ],
     });
   },
