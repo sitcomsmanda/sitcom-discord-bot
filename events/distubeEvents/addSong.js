4d9bc9013d5f9d07b3d7b770654e1a69a8b84edd
DikDns <dikdnssocial@gmail.com>
2022-06-10 14:25:24 +0700

    add 'add song' event

---
+++ events/distubeEvents/addSong.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  // Nama Event: guildMemberRemove
+  name: "playSong",
+  // Method isi Event guildMemberRemove
+  async execute(queue, song) {
+    await queue.textChannel.send({
+      embeds: [
+        new MessageEmbed()
+          .setColor("#43E97B")
+          .setDescription(
+            `🟢 | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+          ),
+      ],
+    });
+  },
+};
