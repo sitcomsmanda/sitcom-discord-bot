e5ebc83d72aaab57c40f8696d81fcf0f0b79c667
DikDns <dikdnssocial@gmail.com>
2022-06-10 14:21:45 +0700

    add distubeStatus queue

---
+++ modules/distubeStatus.js
@@ -0,0 +1,12 @@
+const status = (queue) =>
+  `Volume: \`${queue.volume}%\` | Filter: \`${
+    queue.filters.join(", ") || "Off"
+  }\` | Loop: \`${
+    queue.repeatMode
+      ? queue.repeatMode === 2
+        ? "All Queue"
+        : "This Song"
+      : "Off"
+  }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+
+module.exports = status;
