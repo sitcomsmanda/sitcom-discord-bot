225f635a803871ca5e14d636f62f1299886d284b
DikDns <dikdnssocial@gmail.com>
2022-06-10 14:37:07 +0700

    add finish event

---
+++ events/distubeEvents/finish.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "finish",
+  async execute(queue) {
+    await queue.textChannel.send({
+      embeds: [
+        new MessageEmbed().setColor("#43E97B").setDescription("Finished!"),
+      ],
+    });
+  },
+};
