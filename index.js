c5bb4ff7ffaf3c37f93cd495c2eadea3fea7fada
DikDns <dikdnssocial@gmail.com>
2022-06-30 11:54:45 +0700

    add: GUILD_ID example and console.log modif

--- index.js
+++ index.js
@@ -19,109 +19,121 @@ const CLIENT_ID_SPOTIFY = process.env.CLIENT_ID_SPOTIFY;
 const CLIENT_SECRET_SPOTIFY = process.env.CLIENT_SECRET_SPOTIFY;
 
 const main = async () => {
-  // Opsi Intens
-  const options = {
-    intents: [
-      Discord.Intents.FLAGS.GUILDS,
-      Discord.Intents.FLAGS.GUILD_MEMBERS,
-      Discord.Intents.FLAGS.GUILD_MESSAGES,
-      Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-      Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-      Discord.Intents.FLAGS.GUILD_PRESENCES,
-      Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-      Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-    ],
-    allowedMentions: {
-      parse: ["users", "roles"],
-    },
-  };
+	// Opsi Intens
+	const options = {
+		intents: [
+			Discord.Intents.FLAGS.GUILDS,
+			Discord.Intents.FLAGS.GUILD_MEMBERS,
+			Discord.Intents.FLAGS.GUILD_MESSAGES,
+			Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+			Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+			Discord.Intents.FLAGS.GUILD_PRESENCES,
+			Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+			Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+		],
+		allowedMentions: {
+			parse: ["users", "roles"],
+		},
+	};
 
-  // Inisialisasi object client utama
-  const client = new Discord.Client(options);
+	// Inisialisasi object client utama
+	const client = new Discord.Client(options);
 
-  // Membuat property prefix untuk diakses di file lain
-  client.prefix = PREFIX;
-  // Membuat property slashCommands
-  client.slashCommands = new Discord.Collection();
-  // Membuat property messageCommands
-  client.messageCommands = new Discord.Collection();
-  // Membuat property musicCommands
-  client.musicCommands = new Discord.Collection();
+	// Overwrite Console.log method
+	console.log = async (msg) => {
+		process.stdout.write(msg + "\n");
 
-  /**
-   *! MEMBACA DIREKTORI ./commands/slash (Slash Commands)
-   **/
-  handleFiles("commands/slash").forEach((command) => {
-    client.slashCommands.set(command.data.name, command);
-  });
+		const ch = client.channels.cache.find(
+			(channel) => channel.name === "sitcom-bot-log"
+		);
 
-  /**
-   *! MEMBACA DIREKTORI ./commands/message (Message Commands)
-   **/
-  handleFiles("commands/message").forEach((messageCommand) => {
-    const commandName = messageCommand.data.name;
-    messageCommand.data.name = PREFIX + commandName;
-    client.messageCommands.set(messageCommand.data.name, messageCommand);
-  });
+		if (!ch) return;
+		await ch.send(msg);
+	};
 
-  /**
-   *! MEMBACA DIREKTORI ./commands/music (MUSIC COMMANDS HANDLER)
-   **/
-  handleFiles("commands/music").forEach((musicCommand) => {
-    const commandName = musicCommand.data.name;
-    musicCommand.data.name = PREFIX + commandName;
-    client.musicCommands.set(musicCommand.data.name, musicCommand);
-  });
+	// Membuat property prefix untuk diakses di file lain
+	client.prefix = PREFIX;
+	// Membuat property slashCommands
+	client.slashCommands = new Discord.Collection();
+	// Membuat property messageCommands
+	client.messageCommands = new Discord.Collection();
+	// Membuat property musicCommands
+	client.musicCommands = new Discord.Collection();
 
-  /**
-   *! MEMBACA DIREKTORI ./events/discord (Discord Events Handler)
-   **/
-  handleFiles("events/discord").forEach((event) => {
-    if (event.once) {
-      // Jika tipe event adalah once
-      client.once(event.name, (...args) => event.execute(...args));
-    } else {
-      // Jika tipe event adalah on
-      client.on(event.name, (...args) => event.execute(...args));
-    }
-  });
+	/**
+	 *! MEMBACA DIREKTORI ./commands/slash (Slash Commands)
+	 **/
+	handleFiles("commands/slash").forEach((command) => {
+		client.slashCommands.set(command.data.name, command);
+	});
 
-  // Opsi DisTube
-  const distubeOptions = {
-    youtubeDL: false,
-    updateYouTubeDL: false,
-    emitNewSongOnly: true,
-    emitAddSongWhenCreatingQueue: false,
-    leaveOnFinish: true,
-    plugins: [
-      new YtDlpPlugin(),
-      new SpotifyPlugin({
-        parallel: true,
-        emitEventsAfterFetching: false,
-        api: {
-          clientId: CLIENT_ID_SPOTIFY,
-          clientSecret: CLIENT_SECRET_SPOTIFY,
-        },
-      }),
-    ],
-  };
+	/**
+	 *! MEMBACA DIREKTORI ./commands/message (Message Commands)
+	 **/
+	handleFiles("commands/message").forEach((messageCommand) => {
+		const commandName = messageCommand.data.name;
+		messageCommand.data.name = PREFIX + commandName;
+		client.messageCommands.set(messageCommand.data.name, messageCommand);
+	});
 
-  client.distube = new DisTube(client, distubeOptions);
+	/**
+	 *! MEMBACA DIREKTORI ./commands/music (MUSIC COMMANDS HANDLER)
+	 **/
+	handleFiles("commands/music").forEach((musicCommand) => {
+		const commandName = musicCommand.data.name;
+		musicCommand.data.name = PREFIX + commandName;
+		client.musicCommands.set(musicCommand.data.name, musicCommand);
+	});
 
-  /**
-   *! MEMBACA DIREKTORI ./events/distube (DisTube Events Handler)
-   **/
-  handleFiles("events/distube").forEach((distubeEvent) => {
-    client.distube.on(distubeEvent.name, (...args) =>
-      distubeEvent.execute(...args)
-    );
-  });
+	/**
+	 *! MEMBACA DIREKTORI ./events/discord (Discord Events Handler)
+	 **/
+	handleFiles("events/discord").forEach((event) => {
+		if (event.once) {
+			// Jika tipe event adalah once
+			client.once(event.name, (...args) => event.execute(...args));
+		} else {
+			// Jika tipe event adalah on
+			client.on(event.name, (...args) => event.execute(...args));
+		}
+	});
 
-  await client.login(TOKEN);
+	// Opsi DisTube
+	const distubeOptions = {
+		youtubeDL: false,
+		updateYouTubeDL: false,
+		emitNewSongOnly: true,
+		emitAddSongWhenCreatingQueue: false,
+		leaveOnFinish: true,
+		plugins: [
+			new YtDlpPlugin(),
+			new SpotifyPlugin({
+				parallel: true,
+				emitEventsAfterFetching: false,
+				api: {
+					clientId: CLIENT_ID_SPOTIFY,
+					clientSecret: CLIENT_SECRET_SPOTIFY,
+				},
+			}),
+		],
+	};
+
+	client.distube = new DisTube(client, distubeOptions);
+
+	/**
+	 *! MEMBACA DIREKTORI ./events/distube (DisTube Events Handler)
+	 **/
+	handleFiles("events/distube").forEach((distubeEvent) => {
+		client.distube.on(distubeEvent.name, (...args) =>
+			distubeEvent.execute(...args)
+		);
+	});
+
+	await client.login(TOKEN);
 };
 
 try {
-  main();
+	main();
 } catch (error) {
-  console.error(error);
+	console.error(error);
 }
