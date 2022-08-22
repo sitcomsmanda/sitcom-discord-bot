54c70bf7ff2cc28e81e780473bc05d95c1c06128
DikDns <dikdnssocial@gmail.com>
2022-08-22 17:55:04 +0700

    add: JSON stringfy in the console.log

--- index.js
+++ index.js
@@ -19,128 +19,129 @@ const CLIENT_ID_SPOTIFY = process.env.CLIENT_ID_SPOTIFY;
 const CLIENT_SECRET_SPOTIFY = process.env.CLIENT_SECRET_SPOTIFY;
 
 const main = async () => {
-	// Opsi Intens
-	const options = {
-		intents: [
-			Discord.Intents.FLAGS.GUILDS,
-			Discord.Intents.FLAGS.GUILD_MEMBERS,
-			Discord.Intents.FLAGS.GUILD_MESSAGES,
-			Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-			Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-			Discord.Intents.FLAGS.GUILD_PRESENCES,
-			Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-			Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-		],
-		allowedMentions: {
-			parse: ["users", "roles"],
-		},
-	};
+  // Opsi Intens
+  const options = {
+    intents: [
+      Discord.Intents.FLAGS.GUILDS,
+      Discord.Intents.FLAGS.GUILD_MEMBERS,
+      Discord.Intents.FLAGS.GUILD_MESSAGES,
+      Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+      Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      Discord.Intents.FLAGS.GUILD_PRESENCES,
+      Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+      Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    ],
+    allowedMentions: {
+      parse: ["users", "roles"],
+    },
+  };
 
-	// Inisialisasi object client utama
-	const client = new Discord.Client(options);
+  // Inisialisasi object client utama
+  const client = new Discord.Client(options);
 
-	// Overwrite Console.log method
-	console.log = async (msg) => {
-		process.stdout.write(msg + "\n");
+  // Overwrite Console.log method
+  console.log = async (msg) => {
+    process.stdout.write(JSON.stringify(msg));
+    process.stdout.write("\n");
 
-		const ch = client.channels.cache.find(
-			(channel) => channel.name === "sitcom-bot-log"
-		);
+    const ch = client.channels.cache.find(
+      (channel) => channel.name === "sitcom-bot-log"
+    );
 
-		if (!ch) return;
+    if (!ch) return;
 
-		const embed = new Discord.MessageEmbed()
-			.setColor("DARKER_GREY")
-			.setTitle(`Console:`)
-			.setDescription(msg)
-			.setTimestamp();
+    const embed = new Discord.MessageEmbed()
+      .setColor("DARKER_GREY")
+      .setTitle(`Console:`)
+      .setDescription(JSON.stringify(msg))
+      .setTimestamp();
 
-		await ch.send({ content: ".", embeds: [embed] });
-	};
+    await ch.send({ content: ".", embeds: [embed] });
+  };
 
-	// Membuat property prefix untuk diakses di file lain
-	client.prefix = PREFIX;
-	// Membuat property slashCommands
-	client.slashCommands = new Discord.Collection();
-	// Membuat property messageCommands
-	client.messageCommands = new Discord.Collection();
-	// Membuat property musicCommands
-	client.musicCommands = new Discord.Collection();
+  // Membuat property prefix untuk diakses di file lain
+  client.prefix = PREFIX;
+  // Membuat property slashCommands
+  client.slashCommands = new Discord.Collection();
+  // Membuat property messageCommands
+  client.messageCommands = new Discord.Collection();
+  // Membuat property musicCommands
+  client.musicCommands = new Discord.Collection();
 
-	/**
-	 *! MEMBACA DIREKTORI ./commands/slash (Slash Commands)
-	 **/
-	handleFiles("commands/slash").forEach((command) => {
-		client.slashCommands.set(command.data.name, command);
-	});
+  /**
+   *! MEMBACA DIREKTORI ./commands/slash (Slash Commands)
+   **/
+  handleFiles("commands/slash").forEach((command) => {
+    client.slashCommands.set(command.data.name, command);
+  });
 
-	/**
-	 *! MEMBACA DIREKTORI ./commands/message (Message Commands)
-	 **/
-	handleFiles("commands/message").forEach((messageCommand) => {
-		const commandName = messageCommand.data.name;
-		messageCommand.data.name = PREFIX + commandName;
-		client.messageCommands.set(messageCommand.data.name, messageCommand);
-	});
+  /**
+   *! MEMBACA DIREKTORI ./commands/message (Message Commands)
+   **/
+  handleFiles("commands/message").forEach((messageCommand) => {
+    const commandName = messageCommand.data.name;
+    messageCommand.data.name = PREFIX + commandName;
+    client.messageCommands.set(messageCommand.data.name, messageCommand);
+  });
 
-	/**
-	 *! MEMBACA DIREKTORI ./commands/music (MUSIC COMMANDS HANDLER)
-	 **/
-	handleFiles("commands/music").forEach((musicCommand) => {
-		const commandName = musicCommand.data.name;
-		musicCommand.data.name = PREFIX + commandName;
-		client.musicCommands.set(musicCommand.data.name, musicCommand);
-	});
+  /**
+   *! MEMBACA DIREKTORI ./commands/music (MUSIC COMMANDS HANDLER)
+   **/
+  handleFiles("commands/music").forEach((musicCommand) => {
+    const commandName = musicCommand.data.name;
+    musicCommand.data.name = PREFIX + commandName;
+    client.musicCommands.set(musicCommand.data.name, musicCommand);
+  });
 
-	/**
-	 *! MEMBACA DIREKTORI ./events/discord (Discord Events Handler)
-	 **/
-	handleFiles("events/discord").forEach((event) => {
-		if (event.once) {
-			// Jika tipe event adalah once
-			client.once(event.name, (...args) => event.execute(...args));
-		} else {
-			// Jika tipe event adalah on
-			client.on(event.name, (...args) => event.execute(...args));
-		}
-	});
+  /**
+   *! MEMBACA DIREKTORI ./events/discord (Discord Events Handler)
+   **/
+  handleFiles("events/discord").forEach((event) => {
+    if (event.once) {
+      // Jika tipe event adalah once
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      // Jika tipe event adalah on
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  });
 
-	// Opsi DisTube
-	const distubeOptions = {
-		youtubeDL: false,
-		updateYouTubeDL: false,
-		emitNewSongOnly: true,
-		emitAddSongWhenCreatingQueue: false,
-		leaveOnFinish: true,
-		plugins: [
-			new YtDlpPlugin(),
-			new SpotifyPlugin({
-				parallel: true,
-				emitEventsAfterFetching: false,
-				api: {
-					clientId: CLIENT_ID_SPOTIFY,
-					clientSecret: CLIENT_SECRET_SPOTIFY,
-				},
-			}),
-		],
-	};
+  // Opsi DisTube
+  const distubeOptions = {
+    youtubeDL: false,
+    updateYouTubeDL: false,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    leaveOnFinish: true,
+    plugins: [
+      new YtDlpPlugin(),
+      new SpotifyPlugin({
+        parallel: true,
+        emitEventsAfterFetching: false,
+        api: {
+          clientId: CLIENT_ID_SPOTIFY,
+          clientSecret: CLIENT_SECRET_SPOTIFY,
+        },
+      }),
+    ],
+  };
 
-	client.distube = new DisTube(client, distubeOptions);
+  client.distube = new DisTube(client, distubeOptions);
 
-	/**
-	 *! MEMBACA DIREKTORI ./events/distube (DisTube Events Handler)
-	 **/
-	handleFiles("events/distube").forEach((distubeEvent) => {
-		client.distube.on(distubeEvent.name, (...args) =>
-			distubeEvent.execute(...args)
-		);
-	});
+  /**
+   *! MEMBACA DIREKTORI ./events/distube (DisTube Events Handler)
+   **/
+  handleFiles("events/distube").forEach((distubeEvent) => {
+    client.distube.on(distubeEvent.name, (...args) =>
+      distubeEvent.execute(...args)
+    );
+  });
 
-	await client.login(TOKEN);
+  await client.login(TOKEN);
 };
 
 try {
-	main();
+  main();
 } catch (error) {
-	console.error(error);
+  console.error(error);
 }
